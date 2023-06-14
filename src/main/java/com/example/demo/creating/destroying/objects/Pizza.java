bad18ba6c4d630635837b4c63311ab516fdfa1eb
kklimek <klimek.krzy@gmail.com>
2023-06-14 15:17:31 +0200

    Item 2: Consider a builder when faced with many constructor parameters

---
+++ src/main/java/com/example/demo/creating/destroying/objects/Pizza.java
@@ -0,0 +1,35 @@
+package com.example.demo.creating.destroying.objects;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+// Builder pattern for class hierarchies
+//		Note that the build method in each subclass’s builder is declared to return the
+//		correct subclass: the build method of NyPizza.Builder returns NyPizza , while
+//		the one in Calzone.Builder returns Calzone . This technique, wherein a subclass
+//		method is declared to return a subtype of the return type declared in the superclass,
+//		is known as covariant return typing. It allows clients to use these builders
+//		without the need for casting.
+public class Pizza {
+	public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+	final Set<Topping> toppings;
+
+	abstract static class Builder<T extends Builder<T>> {
+		EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+		public T addTopping(Topping topping) {
+			toppings.add(Objects.requireNonNull(topping));
+			return self();
+		}
+
+		abstract Pizza build();
+
+		// Subclasses must override this method to return "this"
+		protected abstract T self();
+	}
+
+	Pizza(Builder<?> builder) {
+		toppings = builder.toppings.clone();
+	}
+}
---
+++ src/main/java/com/example/demo/creating/destroying/objects/PizzaCalzone.java
@@ -0,0 +1,35 @@
+package com.example.demo.creating.destroying.objects;
+
+//		Note that the build method in each subclass’s builder is declared to return the
+//		correct subclass: the build method of NyPizza.Builder returns NyPizza , while
+//		the one in Calzone.Builder returns Calzone . This technique, wherein a subclass
+//		method is declared to return a subtype of the return type declared in the superclass,
+//		is known as covariant return typing. It allows clients to use these builders
+//		without the need for casting.
+public class PizzaCalzone extends Pizza{
+	private final boolean sauceInside;
+
+	public static class Builder extends Pizza.Builder<Builder>{
+		private boolean sauceInside = false; // Default
+
+		public Builder sauceInside(){
+			this.sauceInside = true;
+			return this;
+		}
+
+		@Override
+		public PizzaCalzone build(){
+			return new PizzaCalzone(this);
+		}
+
+		@Override
+		protected Builder self(){
+			return this;
+		}
+	}
+
+	public PizzaCalzone(Builder builder){
+		super(builder);
+		this.sauceInside = builder.sauceInside;
+	}
+}
