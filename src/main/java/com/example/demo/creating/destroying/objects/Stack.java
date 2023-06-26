433921b161d79e5c8d655168b0d6f68ab22c4beb
kklimek <klimek.krzy@gmail.com>
2023-06-26 14:49:19 +0200

    Item 13: Override clone judiciously

--- src/main/java/com/example/demo/creating/destroying/objects/Stack.java
+++ src/main/java/com/example/demo/creating/destroying/objects/Stack.java
@@ -33,4 +33,27 @@ public class Stack {
 		if (elements.length == size)
 			elements = Arrays.copyOf(elements, 2 * size + 1);
 	}
+
+// 	Clone method for class with references to mutable state. If the clone method merely  returns super.clone(), the
+// 	resulting Stack instance will have the correct value in its size field, but its elements field will refer to the
+// 	same array as the original Stack instance. Modifying the original will destroy the invariants in the clone and
+//	vice versa. You will quickly find that your program produces nonsensical results or throws a NullPointerException.
+//	Note that we do not have to cast the result of elements.clone to Object[]. Calling clone on an array returns an
+//	array whose runtime and compile-time types are identical to those of the array being cloned. This is the preferred
+//	idiom to duplicate an array. In fact, arrays are the sole compelling use of the clone facility. Note also that the
+//	earlier solution would not work if the elements field were final because clone would be prohibited from assigning
+//	a new value to the field. This is a fundamental problem: like serialization, the Cloneable architecture is
+//	incompatible with normal use of final fields referring to mutable objects, except in cases where the mutable objects
+//	may be safely shared between an object and its clone. In order to make a class cloneable, it may be necessary to
+//	remove final modifiers from some fields.
+	@Override
+	public Stack clone() {
+		try {
+			Stack result = (Stack) super.clone();
+			result.elements = elements.clone();
+			return result;
+		} catch (CloneNotSupportedException e) {
+			throw new AssertionError();
+		}
+	}
 }
