672325a45ce6eab9fd2efbf21fc894e2ced38667
kklimek <klimek.krzy@gmail.com>
2023-07-10 17:30:29 +0200

    Item 34: Use enums instead of int constants

---
+++ src/main/java/com/example/demo/enums/and/annotations/PayrollDay.java
@@ -0,0 +1,47 @@
+package com.example.demo.enums.and.annotations;
+
+public enum PayrollDay {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY(PayType.WEEKEND),
+	SUNDAY(PayType.WEEKEND);
+
+	private final PayType payType;
+
+	PayrollDay() {
+		this.payType = PayType.WEEKDAY;
+	}
+
+	PayrollDay(PayType payType) {
+		this.payType = payType;
+	}
+
+	int pay(int minutesWorked, int payRate) {
+		return payType.pay(minutesWorked, payRate);
+	}
+
+	private enum PayType {
+		WEEKDAY {
+			int overtimePay(int minsWorked, int payRate) {
+				return minsWorked <= MINS_PER_SHIFT ? 0 : (minsWorked - MINS_PER_SHIFT) * payRate / 2;
+			}
+		},
+		WEEKEND {
+			int overtimePay(int minsWorked, int payRate) {
+				return minsWorked * payRate / 2;
+			}
+		};
+
+		abstract int overtimePay(int minsWorked, int payRate);
+
+		private static final int MINS_PER_SHIFT = 8 * 60;
+
+		int pay(int minsWorked, int payRate) {
+			int basePay = minsWorked * payRate;
+			return basePay + overtimePay(minsWorked, payRate);
+		}
+	}
+}
